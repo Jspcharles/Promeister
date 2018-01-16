ebbb1d4e8e30dcdf025befc9375f4e83c8a2934a
Joseph Charles <Jspcharles@gmail.com>
2018-01-17 00:58:39 +0530

    Some changes

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content') 
-        <h1>{{$title}}</h1>     
+        <h1>{{$title}}</h1>    
+        <h1>{{$title}}</h1> 
        <h3>My First Laravel Project</h3>
  @endsection
